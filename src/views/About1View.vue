d21ffe331f27f3cbeed6b06326fe3850dd328a7a
GamingYoung <zzzz7057@163.com>
2023-02-04 17:09:49 +0800

    mTJ

---
+++ src/views/About1View.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <svg width="100%" height="100%"></svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about1View'
+}
+</script>
+
+<style scoped>
+
+</style>
